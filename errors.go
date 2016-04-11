6d035593f0f762473343a9ba56e99141135a65f3
wlMalk <wm.wexem@gmail.com>
2016-04-11 15:24:44 -0500

    Improved validation package and added some basic validators

--- errors.go
+++ errors.go
@@ -38,3 +38,31 @@ func (e Error) UnionAppend(err Error) {
 		e[k] = append(e[k], err[k]...)
 	}
 }
+
+type Err string
+
+func (err Err) Err(vals ...interface{}) error {
+	return ValidationErr(fmt.Sprintf(string(err), vals...))
+}
+
+type Msg string
+
+func (m Msg) Msg(vals ...interface{}) string {
+	return fmt.Sprintf(string(m), vals...)
+}
+
+type ValidationErr string
+
+func (err ValidationErr) Error() string {
+	return string(err)
+}
+
+const (
+	ErrEq     = Err("%v must equal %v") // prefix "Validation Error: " will be added
+	ErrRegexp = Err("%v must match %v")
+)
+
+const (
+	MsgEq     = Msg("must equal %v") // prefix "Validation Error: " will be added
+	MsgRegexp = Msg("must match %v")
+)
---
+++ validators.go
@@ -0,0 +1,52 @@
+package validation
+
+import (
+	"regexp"
+
+	"github.com/wlMalk/mirango/framework"
+)
+
+var (
+	RegexpNumber = regexp.MustCompile("[0-9]+")
+	RegexpAlpha  = regexp.MustCompile("[a-zA-Z]+")
+)
+
+func Eq(a interface{}) Validator {
+	return NewFuncValidator(func(c framework.Context, v framework.ParamValue) error {
+		if a != v.Value() {
+			return ErrEq.Err([]interface{}{v.Name(), a}...)
+		}
+		return nil
+	}, MsgEq.Msg(a))
+}
+
+func Number() Validator {
+	return Regexp(RegexpNumber)
+}
+
+func Alpha() Validator {
+	return Regexp(RegexpAlpha)
+}
+
+func Regexp(p *regexp.Regexp) Validator {
+	return NewFuncValidator(func(c framework.Context, v framework.ParamValue) error {
+		if !p.MatchString(v.RawString()) {
+			return ErrRegexp.Err([]interface{}{v.Name(), p.String()}...)
+		}
+		return nil
+	}, MsgRegexp.Msg(p.String()))
+}
+
+func StringRegexp(pstr string) Validator {
+	p, err := regexp.Compile(pstr)
+	if err != nil {
+		return nil
+	}
+	return NewFuncValidator(func(c framework.Context, v framework.ParamValue) error {
+		if !p.MatchString(v.RawString()) {
+			return ErrRegexp.Err([]interface{}{v.Name(), p.String()}...)
+		}
+		return nil
+	}, MsgRegexp.Msg(p.String()))
+}
+
